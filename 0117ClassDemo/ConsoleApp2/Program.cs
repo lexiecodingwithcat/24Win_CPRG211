637bee7f120ccb7d5ec204b429efe8226079ddc8
Tianzi Cui <cuitianzi0@gmail.com>
2024-01-17 09:56:35 -0700

    Fizzbuzz exercise using static keywords

--- 0117ClassDemo/ConsoleApp2/Program.cs
+++ 0117ClassDemo/ConsoleApp2/Program.cs
@@ -7,16 +7,34 @@ using System.Threading.Tasks;
 namespace ConsoleApp2
     {
 
-    static class Person
+    class Person //static class
         {
+        //static data members
         public static string Name = "Steven";
+        public static void Display() //static function 
+            {
+            Console.WriteLine(Name);
 
-
+            }
+       static  Person() //static constructor
+            {
+            Console.WriteLine("This is the static constructor");
+            }
         }
      class Program
         {
         static void Main(string[] args) //main method
             {
+            //static makes variables, methods and functions accessible without object creation
+            //public still need  object creation 
+            Person.Name = "hello";
+            Console.WriteLine("output is " + Person.Name);
+            Person.Display();
+
+            //consturctor need an object to be evoked, so moved the static keyword before the class
+            Person obj1 = new Person();
+
+            Console.ReadLine();
             }
         }
     }
---
+++ 0117ClassDemo/ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+    {
+    class Program
+        {
+        static class FizzBuzz
+            {
+            static public int number = 1;
+            static public void Fizzbuzz()
+                {
+                for (int i = number; i <= 100; i++)
+                    {
+                    if (i % 3 == 0)
+                        {
+                        if (i % 5 == 0)
+                            {
+                            Console.WriteLine($"{i}FizzBuzz");
+                            }
+                        else
+                            {
+                            Console.WriteLine($"{i}Fizz");
+                            }
+
+                        }
+                    else if (i % 5 == 0)
+                        {
+                        Console.WriteLine($"{i}Buzz");
+                        }
+                    else
+                        {
+                        Console.WriteLine(i);
+                        }
+                    }
+                }
+            }
+        static void Main(string[] args)
+            {
+            FizzBuzz.Fizzbuzz();
+           
+
+            Console.ReadLine();
+            }
+        }
+    }
