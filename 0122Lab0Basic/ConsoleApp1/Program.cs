c93bc3234244b46273356b9d8f4b5370cae2fda0
Tianzi Cui <cuitianzi0@gmail.com>
2024-01-17 20:54:45 -0700

    add file I/O

--- 0122Lab0Basic/ConsoleApp1/Program.cs
+++ 0122Lab0Basic/ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.Remoting.Contexts;
 
 
@@ -9,31 +11,46 @@ namespace ConsoleApp1
         {
         static void Main(string[] args)
             {
+            //take input from user
             Console.WriteLine("Please enter the first number(greater than 0):");
-            int lowNumber = Convert.ToInt32(Console.ReadLine());
+            double lowNumber = Convert.ToDouble(Console.ReadLine());
+
             while (lowNumber < 0)
                 {
                 Console.WriteLine("The number you input is less than 0. Please enter a number greater than 0 :");
                 lowNumber = Convert.ToInt32(Console.ReadLine());
                 }
             Console.WriteLine("Please enter the second number:");
-            int highNumber = Convert.ToInt32(Console.ReadLine());
+            double highNumber = Convert.ToDouble(Console.ReadLine());
             while (highNumber < lowNumber)
                 {
                 Console.WriteLine("Please enter the second number which is greater the first number:");
                  highNumber = Convert.ToInt32(Console.ReadLine());
                 }
-            int difference = highNumber - lowNumber;
-            int[] numbers = new int[difference+1];
-            int index = 0;
-            for (int i = highNumber; i > lowNumber; i--)
+            double difference = highNumber - lowNumber;
+            //create a list 
+            List <string> result = new List <string>();
+            for (double i = highNumber; i >= lowNumber; i-=0.1)
                 {
-                numbers[index] = i;
-                //Console.WriteLine(numbers[index]);
-                index++;
-               
+          //learned this from chatGPT because using double will have many float numbers after dot
+          //such as 15.6999999999
+            i = Math.Round(i,1);
+                // add number between low number and high number to the list 
+                result.Add(i.ToString());
+                Console.WriteLine(i);
                 }
+            string filepath = @"D:\SAIT\c#\0122Lab0Basic\numbers.txt";
+            File.AppendAllLines(filepath, result);
 
+            //Read the numbers back from the file and print out the sum of the numbers.
+            result = File.ReadAllLines(filepath).ToList();
+            double total = 0;
+            foreach (string line in result)
+                {
+                double number = Convert.ToDouble(line);
+                total += number;
+                }
+            Console.WriteLine(total);
 
 
             Console.ReadLine();
