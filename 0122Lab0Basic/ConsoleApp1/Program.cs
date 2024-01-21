559da5d464a4cc42f540b0827e2ca9d4c9d1927f
Tianzi Cui <cuitianzi0@gmail.com>
2024-01-21 14:49:44 -0700

    add statement for prime number

--- 0122Lab0Basic/ConsoleApp1/Program.cs
+++ 0122Lab0Basic/ConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.Remoting.Contexts;
+
 
 
 namespace ConsoleApp1
@@ -14,7 +14,7 @@ namespace ConsoleApp1
             //take input from user
             Console.WriteLine("Please enter the first number(greater than 0):");
             double lowNumber = Convert.ToDouble(Console.ReadLine());
-
+            //loop until the input number is positive 
             while (lowNumber < 0)
                 {
                 Console.WriteLine("The number you input is less than 0. Please enter a number greater than 0 :");
@@ -22,35 +22,67 @@ namespace ConsoleApp1
                 }
             Console.WriteLine("Please enter the second number:");
             double highNumber = Convert.ToDouble(Console.ReadLine());
+            //loop until the input number is greater than the first number
             while (highNumber < lowNumber)
                 {
                 Console.WriteLine("Please enter the second number which is greater the first number:");
                  highNumber = Convert.ToInt32(Console.ReadLine());
                 }
             double difference = highNumber - lowNumber;
+            Console.WriteLine($"The difference between {highNumber} and {lowNumber} is {difference}");
+
             //create a list 
             List <string> result = new List <string>();
-            for (double i = highNumber; i >= lowNumber; i-=0.1)
+
+            //convert double to int 
+            int number1 = Convert.ToInt32(highNumber);
+            int number2 = Convert.ToInt32(lowNumber);
+            for (int i = number1; i >= number2; i--)
                 {
-          //learned this from chatGPT because using double will have many float numbers after dot
-          //such as 15.6999999999
-            i = Math.Round(i,1);
+        
                 // add number between low number and high number to the list 
                 result.Add(i.ToString());
-                Console.WriteLine(i);
+               
                 }
             string filepath = @"D:\SAIT\c#\0122Lab0Basic\numbers.txt";
-            File.AppendAllLines(filepath, result);
+            File.WriteAllLines(filepath, result);
 
             //Read the numbers back from the file and print out the sum of the numbers.
             result = File.ReadAllLines(filepath).ToList();
-            double total = 0;
+           int total = 0;
+            Boolean flag = true;
             foreach (string line in result)
                 {
-                double number = Convert.ToDouble(line);
-                total += number;
+                int eachLine = Convert.ToInt32(line);
+                total += eachLine;
+                if (eachLine > 3)
+                    {
+                    for (int i = 2; i < eachLine; i++)
+                        {
+                        if (eachLine % i == 0)
+                            {
+                            flag = false;
+                            break;
+                            }
+                        else { flag = true; }
+                        }
+                    }
+                if (flag)
+                    {
+                    Console.WriteLine($"{eachLine} is a prime number");
+                    }
+                else
+                    {
+                    Console.WriteLine($"{eachLine} is not a prime number");
+                    }
+                
+                    
+
                 }
-            Console.WriteLine(total);
+
+            Console.WriteLine($"The total number in the list is {total}");
+
+
 
 
             Console.ReadLine();
