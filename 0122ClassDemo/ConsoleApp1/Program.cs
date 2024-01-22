f02eca80874879640e0e321d019545011d796c6f
Tianzi Cui <cuitianzi0@gmail.com>
2024-01-22 10:19:25 -0700

    inheritence of the class

---
+++ 0122ClassDemo/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ConsoleApp1
+    {
+    class ParentClass
+        {
+        public void Display()
+            {
+            Console.WriteLine("parent class called");
+            }
+        //if you want the child class to override this function
+        //keyword should be written
+        //protected void Print()
+        public virtual void Print()
+            {
+            Console.WriteLine("I am in parent class");
+            }
+        }
+
+    class Childclass : ParentClass {
+        //by using override and virtual keywords, this function is override the parent function with the same name
+        //when calling the obj created by childclass, only this one will be invoked 
+        public override void Print() { 
+            //this proteced function can only be accessed through same class or derived/ child class
+            //like private function but only used in inheritence
+            //PrintMessage();
+
+            Console.WriteLine("Child class called");
+            }
+        
+        }
+
+    class Class3: Childclass { 
+        public void Printdata()
+            {
+            Console.WriteLine("Class 3 called");
+            }
+        
+        
+        }
+    
+
+    class Program
+        {
+        static void Main(string[] args)
+            {
+
+            //Childclass child1 = new Childclass();//obj of child class
+            //child1.Display(); //parent class method invoked
+            //child1.PrintMessage(); //parent class method
+            //child1.Print();//child class method
+
+            //multi-level inheritence
+            Class3 child2 = new Class3();
+            //child2.Display(); //inherited from Parentclass
+            //child2.PrintMessage(); //this will not work cuz this is protected
+            child2.Print();//inherited from Childclass
+            //child2.Printdata();//method from itself
+
+            }
+        }
+    }
---
+++ 0122ClassDemo/ConsoleApp2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+namespace ConsoleApp2
+    {
+    class Product
+        {
+        int product_id;
+        public string product_name;
+        public double production_price;
+
+        public int Id
+            {
+            get { return product_id; }
+            set { product_id = value; }
+            }
+
+        public virtual void DisplayProductionInfo()
+            {
+            Console.WriteLine("Information about production");
+            Console.WriteLine($"The product name is {product_name}, the id is {Id}, and the price is {production_price}");
+            }
+        }
+
+    class Electronics : Product
+        {
+        int warrantyMonths;
+
+        public int Warranty
+            {
+            get { return warrantyMonths; }
+            set { warrantyMonths = value; }
+            }
+
+        public override void DisplayProductionInfo()
+            {
+            //both base and child class have the same function name
+            //when you want to override the base function and meanwhile want to call the base function
+            base.DisplayProductionInfo();
+            Console.WriteLine($"The warranty month of this electronic is {warrantyMonths}");
+            }
+
+        }
+
+    class Book : Product
+        {
+        //public string author;
+        string author;
+
+
+        //parameter instuctor
+       public Book(string author)
+            {
+            this.author = author;
+            }
+
+        public override void DisplayProductionInfo()
+            {
+            base.DisplayProductionInfo();
+            Console.WriteLine($"The author of the book is {author}");
+            
+            }
+        }
+
+    class Clothing : Product
+        {
+        public string size;
+        public override void DisplayProductionInfo()
+            {
+            base.DisplayProductionInfo();
+            Console.WriteLine($"The size of the closing is {size}");
+            }
+        }
+
+    class Program
+        {
+
+        static void Main(string[] args)
+            {
+             Electronics phone = new Electronics();
+            phone.Id = 1;
+            phone.product_name = "iPhone 15 Pro";
+            phone.production_price = 2000.56;
+            phone.Warranty = 15;
+            phone.DisplayProductionInfo();
+
+
+            Book harryPotter = new Book("J.K.Rolin");
+            harryPotter.Id = 2;
+           harryPotter.product_name = "magic book";
+           harryPotter.production_price = 50.7;
+           harryPotter.DisplayProductionInfo() ;
+
+            Clothing sweater = new Clothing();
+            sweater.Id = 3;
+            sweater.product_name = "a white sweater";
+            sweater.production_price = 20.1;
+            sweater.size = "Large";
+            sweater.DisplayProductionInfo() ;
+
+
+
+
+            Console.ReadLine();
+
+            }
+        }
+    }
