e20bf5e5c1e07e83c886d32160b5fd5ae6cf2dae
Tianzi Cui <cuitianzi0@gmail.com>
2024-01-24 09:10:49 -0700

    interfaces and multi-level inheritance

--- 0124ClassDemo_abstract/MultipleInterface/Program.cs
+++ 0124ClassDemo_abstract/MultipleInterface/Program.cs
@@ -14,7 +14,7 @@ namespace MultipleInterface
         }
 
     //interfaces should be implemented by class
-    class Democlass:FirstInterface , IFirstInterface 
+    class Democlass: IFirstInterface , ISecondInterface
         { 
        public void MyMethod() {
             Console.WriteLine("Hello, this is the first method");
@@ -23,11 +23,7 @@ namespace MultipleInterface
         public void AnotherMethod() {
             Console.WriteLine("Hello, this is the second method");
             }
-        
-        
-        
-        
-        
+
         }
 
     public class Program
