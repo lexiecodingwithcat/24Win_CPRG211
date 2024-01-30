f50c89407dfa66ae3b595b4d71fae035c3e88057
Tianzi Cui <cuitianzi0@gmail.com>
2024-01-29 21:09:28 -0700

    file read

--- lab2_Inheritance/ConsoleApp1/Program.cs
+++ lab2_Inheritance/ConsoleApp1/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 namespace ConsoleApp1
     {
    class Program
         {
         static void Main(string[] args)
             {
-            
+            //read the file
+            string filePath = @"../../../employees.txt";
+            List <string> result = new List<string>();
+            result = File.ReadAllLines(filePath).ToList();
+            foreach (string line in result)
+                {
+                //split the line 
+                string[] data = line.Split(':');
+                char firstDigit = line[0];
+                Console.WriteLine(data[0]);
+
+
+                
+
+                //if (0<= firstDigit && firstDigit <= 4)
+                //    {
+
+                //    }
+                }
+
+
+
             }
         }
     }
