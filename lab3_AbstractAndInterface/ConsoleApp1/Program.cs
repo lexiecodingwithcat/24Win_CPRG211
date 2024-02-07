c9f5adbf2490237d184155f998615328e65a4910
Tianzi Cui <cuitianzi0@gmail.com>
2024-02-06 20:54:09 -0700

    animal list created

--- lab3_AbstractAndInterface/ConsoleApp1/Program.cs
+++ lab3_AbstractAndInterface/ConsoleApp1/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace ConsoleApp1
     {
    public class Dog : Animal, IAnimal
         {
-        public string Height {  get; set; }
+       public string Height { get; set; }
+        public Dog(string name, string color, string height, int age) {
+            this.Age = age;
+            this.Name = name;
+            this.Color = color;
+            this.Height = height;
+
+                    }
+
         public override void Eat()
             {
             Console.WriteLine("Dogs eat meat.");
@@ -20,10 +29,11 @@ namespace ConsoleApp1
     public class Cat: Animal, IAnimal
         {
         public string Height { get; set; }
-        public Cat(string name, string color, int age)
+        public Cat(string name, string color, string height, int age)
             {
             this.Name = name;
             this.Color = color;
+            this.Height = height;
             this.Age = age;
             }
         public override void Eat()
@@ -42,16 +52,17 @@ namespace ConsoleApp1
         static void Main(string[] args)
             {
             //dog object
-            Dog dog = new Dog();
+           
           Console.WriteLine("What is the name of your dog?");
             string name1 = Console.ReadLine();
-            dog.Name = name1;
+            
             Console.WriteLine("What is the color of your dog?");
-            string color = Console.ReadLine();
-            dog.Color = color;
+            string color1 = Console.ReadLine();
+            Console.WriteLine("How tall is your dog?");
+            string height1 = Console.ReadLine();    
             Console.WriteLine("What is the age of your dog?");
-            int age = int.Parse(Console.ReadLine());
-            dog.Age = age;
+            int age1 = int.Parse(Console.ReadLine());
+            Dog dog = new Dog(name1,color1, height1, age1);
             dog.Eat();
             Console.WriteLine(dog.Cry());
             
@@ -61,12 +72,22 @@ namespace ConsoleApp1
             string name2 = Console.ReadLine() ;
             Console.WriteLine("What is the color of your cat?");
             string color2 = Console.ReadLine() ;
+            Console.WriteLine("How tall is your dog?");
+            string height2 = Console.ReadLine();
             Console.WriteLine("What is the age of your cat?");
             int age2 = int.Parse(Console.ReadLine()) ;
-            Cat cat = new Cat(name2,color2,age2);
+            Cat cat = new Cat(name2,color2,height2,age2);
             cat.Eat();
             Console.WriteLine(cat.Cry());
            
+            //create an animal list
+            List <Animal> animal_list = new List<Animal> { dog, cat};
+            foreach(Animal animal in animal_list)
+                {
+                Console.WriteLine($"The name of the animal is{animal.Name}");
+                }
+
+
             Console.ReadLine();
 
 
