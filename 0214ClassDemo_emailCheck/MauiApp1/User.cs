346a94a1ca18c66d7701ca6ac38e617a704e161e
Tianzi Cui <cuitianzi0@gmail.com>
2024-02-14 09:43:38 -0700

    emailValidation demo finished

---
+++ 0214ClassDemo_emailCheck/MauiApp1/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+    {
+    public class User
+        {
+        int id;
+        string email;
+        public int ID { get { return id; } set { id = value; } }
+        public string Email { get { return email; } set { email = value; } }
+        public User(int id, string email)
+            {
+            this.id = id;
+            this.email = email;
+            }
+
+        }
+    }
---
+++ 0214ClassDemo_emailCheck/MauiApp1/UserManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+    {
+    public class UserManager
+        {
+        string User_text = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "D:\\SAIT\\c#\\0214ClassDemo_emailCheck\\MauiApp1\\Resources\\Res\\users.txt");
+        public static List<User> Users = new List<User>();
+        public UserManager()
+            {
+            populateUsers();
+            }
+        //read data from the file
+        public void populateUsers()
+            {
+            User user;
+            //Read each line from the list
+            foreach (var item in File.ReadAllLines(User_text))
+                {
+                string[] parts = item.Split(",");
+                int id = int.Parse(parts[0]);
+                string email = parts[1];
+                user = new User(id, email);
+                Users.Add(user); //add user into database
+                }
+            }
+        //Return a list
+        public static List <User> GetUsers()
+            {
+            return Users;
+            }
+        
+        }
+    }
